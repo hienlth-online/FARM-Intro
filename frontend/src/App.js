7a3832ee784d1aabff1a0ad64c80e75359e6995c
Hien Luong <hyhien@gmail.com>
2021-02-14 12:21:46 +0700

    Add frontend project to show task UI

---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import "antd/dist/antd.css"
+
+import { CheckCircleOutlined, MinusCircleOutlined } from "@ant-design/icons"
+import { Col, Row, Timeline } from "antd"
+import { useEffect, useState } from "react"
+
+function App() {
+    const [tasks, setTasks] = useState([])
+    const [timeline, setTimeline] = useState([])
+
+    useEffect(() => {
+        const fetchAllTasks = async () => {
+            const response = await fetch("http://localhost:8000/task/")
+            const fetchedTasks = await response.json()
+            setTasks(fetchedTasks)
+        }
+
+        const interval = setInterval(fetchAllTasks, 1000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    useEffect(() => {
+        const timelineItems = tasks.reverse().map((task) => {
+            return task.completed ? (
+                <Timeline.Item
+                    dot={<CheckCircleOutlined />}
+                    color="green"
+                    style={{ textDecoration: "line-through", color: "green" }}
+                >
+                    {task.name} <br/><small>({task._id})</small>
+                </Timeline.Item>
+            ) : (
+                <Timeline.Item
+                    dot={<MinusCircleOutlined />}
+                    color="blue"
+                    style={{ textDecoration: "initial" }}
+                >
+                    {task.name} <br/><small>({task._id})</small>
+                </Timeline.Item>
+            )
+        })
+
+        setTimeline(timelineItems)
+    }, [tasks])
+
+    return (
+        <>
+            <h3 style={{ textAlign: "center"}}>TASKs</h3>
+            <Row style={{ marginTop: 50 }}>
+                <Col span={14} offset={5}>
+                    <Timeline mode="alternate">{timeline}</Timeline>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default App;
