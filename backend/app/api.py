34567953e480103df8e9d1858f68e36b0e2cd076
Hien Luong <hyhien@gmail.com>
2021-02-14 09:03:27 +0700

    Init backend project (FastAPI)

---
+++ backend/app/api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000",
+    "http://localhost:5020",
+    "localhost:5020"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to FARM Stack."}
